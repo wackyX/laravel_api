efc7a4806b559bdb7e151ada143a0798efe3897c
wangxin <wangxin@youshuyun.com>
2020-12-31 14:04:57 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::get('/test', 'ApiController@test');
-Route::get('/', 'ApiController@checkToken');
+Route::get('/check_token', 'ApiController@checkToken');
