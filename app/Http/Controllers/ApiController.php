a432ed67fba059d2a607b8b450236cf5026a88f3
wangxin <wangxin@youshuyun.com>
2021-01-05 15:25:16 +0800

    update

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,6 +4,7 @@
 namespace App\Http\Controllers;
 
 
+use Exception;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Redis;
 use Justmd5\DaTaoKe\Api;
@@ -14,6 +15,11 @@ class ApiController extends Controller
 {
     private $appId = 'wx478e8fc2f38ea123';
     private $appSecret = 'dd5e5f2a2b809fcc46976605f715d43e';
+
+    public $host = '';//请求地址
+    public $dataokeKey = '';//app_key
+    public $dataokeSecret = '';//app_secret
+    public $version = '';//版本号
 
     public function test()
     {
@@ -134,16 +140,12 @@ class ApiController extends Controller
             'version' => 'v1.0.0',
             'appKey'  => env('TAOKOULING_API_KEY'),
         ];
-        include "ApiSdk.php";
-        $dataoke = new \CheckSign();
-        Log::info('aaaaa');
-        $dataoke->host($url);
-        $dataoke->appKey(env('TAOKOULING_API_KEY'));
-        $dataoke->appSecret(env('TAOKOULING_API_SECRET'));
-        $dataoke->version = 'v1.0.0';
-        $params = array();
-        $params['content'] = $str;
-        $data = $dataoke->request($params);
+        $this->host = $url;
+        $this->dataokeKey = env('TAOKOULING_API_KEY');
+        $this->dataokeSecret = env('TAOKOULING_API_SECRET');
+        $this->version = 'v1.0.0';
+        $params = [ 'content' => $str ];
+        $data = $this->request($params);
 //        $data['sign'] = $this->makeSignDataoke($data, env('TAOKOULING_API_SECRET'));
 //        $url = $url . '?' . http_build_query($data);
 //        $res = $this->requestUrl($url);
@@ -179,48 +181,84 @@ class ApiController extends Controller
         return $data;
     }
 
-    public function strCutByStr(&$str, $findStart, $findEnd = false, $encoding = 'utf-8')
-    {
-        if (is_array($findStart)) {
-            if (count($findStart) === count($findEnd)) {
-                foreach ($findStart as $k => $v) {
-                    if (($result = strCutByStr($str, $v, $findEnd[$k], $encoding)) !== false) {
-                        return $result;
-                    }
-                }
-                return false;
-            } else {
-                return false;
-            }
-        }
-
-        if (($start = mb_strpos($str, $findStart, 0, $encoding)) === false) {
-            return false;
-        }
-
-        $start += mb_strlen($findStart, $encoding);
-
-        if ($findEnd === false) {
-            return mb_substr($str, $start, NULL, $encoding);
-        }
-
-        if (($length = mb_strpos($str, $findEnd, $start, $encoding)) === false) {
-            return false;
-        }
-
-        return mb_substr($str, $start, $length - $start, $encoding);
-    }
-
-    private function makeSignDataoke($data, $appSecret)
+    function makeSign($data, $appSecret)
     {
         ksort($data);
         $str = '';
         foreach ($data as $k => $v) {
-
             $str .= '&' . $k . '=' . $v;
         }
         $str = trim($str, '&');
         $sign = strtoupper(md5($str . '&key=' . $appSecret));
         return $sign;
+    }
+
+    function request($params, $type = "GET")
+    {
+        $host = $this->host;
+        $appKey = $this->dataokeKey;
+        $appSecret = $this->dataokeSecret;
+        $version = $this->version;
+        if ($host == '' || $appKey == '' || $appSecret == '' || $version == '') {
+            return json_encode(array( 'code' => -10001, 'msg' => "请完善参数" ));
+        }
+        $type = strtoupper($type);
+        if (!in_array($type, array( "GET", "POST" ))) {
+            return json_encode(array( 'code' => -10001, 'msg' => "只支持GET/POST请求" ));
+        }
+        //默认必传参数
+        $data = [
+            'appKey'  => $appKey,
+            'version' => $version,
+        ];
+        //加密的参数
+        $data = array_merge($params, $data);
+        $data['sign'] = self::makeSign($data, $appSecret);
+        try {
+            if ($type == 'POST') {
+                //拼接请求地址
+                $url = $host;
+                //执行请求获取数据
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                //https调用
+                //curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, 0);
+                //curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);
+                $header = [
+                    'Content-Type: application/json'
+                ];
+                curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+                curl_setopt($ch, CURLOPT_POST, true);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+                $output = curl_exec($ch);
+                $a = curl_error($ch);
+                if (!empty($a)) {
+                    return json_encode(array( 'code' => -10003, 'msg' => $a ));
+                }
+                curl_close($ch);
+                return $output;
+            } else {
+                //拼接请求地址
+                $url = $host . '?' . http_build_query($data);
+                //执行请求获取数据
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+                curl_setopt($ch, CURLOPT_HEADER, 0);
+                $output = curl_exec($ch);
+                $a = curl_error($ch);
+                if (!empty($a)) {
+                    return json_encode(array( 'code' => -10003, 'msg' => $a ));
+                }
+                curl_close($ch);
+                return $output;
+            }
+        } catch (Exception $e) {
+            var_dump($e->getMessage());
+            return json_encode(array( 'code' => -10002, 'msg' => "请求超时或异常，请重试" ));
+        }
     }
 }
