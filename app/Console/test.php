38c42aaceef07ce5a419ffce1b2eb782e5e3f142
wangxin <wangxin@youshuyun.com>
2021-01-21 18:19:11 +0800

    update

--- app/Console/test.php
+++ app/Console/test.php
@@ -8,6 +8,7 @@ use NiuGengYun\EasyTBK\Factory;
 use Illuminate\Console\Command;
 use NiuGengYun\EasyTBK\TaoBao\Request\TbkContentGetRequest;
 use NiuGengYun\EasyTBK\TaoBao\Request\TbkCouponGetRequest;
+use NiuGengYun\EasyTBK\TaoBao\Request\TbkDgMaterialOptionalRequest;
 use NiuGengYun\EasyTBK\TaoBao\Request\TbkItemCouponGetRequest;
 use NiuGengYun\EasyTBK\TaoBao\Request\TbkItemInfoGetRequest;
 use NiuGengYun\EasyTBK\TaoBao\Request\TbkTpwdCreateRequest;
@@ -30,10 +31,10 @@ class test extends Command
     public function handle()
     {
         $client = Factory::taobao();
-        $req = new TbkTpwdCreateRequest();
-        $req->setText("复制内容淘宝打开");
-        $req->setUrl("https://uland.taobao.com/quan/detail?sellerId=1669558588&activityId=8c2bf9951b7a448182eaf89ae4fe2115");
+        $req = new TbkDgMaterialOptionalRequest();
+        $req->setQ('狗狗零食磨牙棒耐咬除口臭补钙幼犬泰迪大型犬金毛拉布拉多牛骨棒');
+        $req->setAdzoneId('mm_1056860041_2202750454_111152500099');
         $data = $client->execute($req);
-        dd($data->data->model);
+        dd($data);
     }
 }
